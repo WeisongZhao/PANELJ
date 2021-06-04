7022db46b6cdc6b5193df7a14f6e994ac81240b0
WeisongZhao <zhaoweisong950713@163.com>
2021-06-04 16:56:52 +0800

    nothing

--- src/main/java/com/HIT/weisongzhao/FRC.java
+++ src/main/java/com/HIT/weisongzhao/FRC.java
@@ -1,8 +1,8 @@
 package com.HIT.weisongzhao;
-/**
- * Created by Weisong on 11/25/2020.
- * Compute the two-frame Fourier Ring Correlation.
- */
+
+//Created by Weisong on 11/25/2020.
+//Compute the two-frame Fourier Ring Correlation.
+
 import java.util.Arrays;
 
 import org.apache.commons.math3.analysis.function.Log;
