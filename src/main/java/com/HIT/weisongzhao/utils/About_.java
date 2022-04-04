90f6d4301f1b0d3702e83e6e73d50875d7fd0699
WeisongZhao <zhaoweisong950713@163.com>
2022-04-04 17:05:31 +0800

    setting merge

--- src/main/java/com/HIT/weisongzhao/utils/About_.java
+++ src/main/java/com/HIT/weisongzhao/utils/About_.java
@@ -7,15 +7,15 @@ import ij.ImageJ;
 import ij.plugin.PlugIn;
 
 public class About_ extends JDialog implements PlugIn {
-	private String defaultMessage = "(c) 2021 	HIT";
+	private String defaultMessage = "(c) 2022 	HIT";
 	private WalkBar walk = new WalkBar(this.defaultMessage, true, false, true);
 
 	@Override
 	public void run(String arg) {
-		this.walk.fillAbout("PANEL error mapping", " 07/19/2021",
+		this.walk.fillAbout("PANEL error mapping", " 03/19/2022",
 				"PANEL: Pixel-level ANalysis of Error Locations <br>v0.2.5",
 				"School of Instrumentation Science and Engineering<br/>Harbin Institute of Technology",
-				"Weisong Zhao (zhaoweisong950713@163.com)", "2021",
+				"Weisong Zhao (zhaoweisong950713@163.com)", "2022",
 				"<p style=\"text-align:left\">" + "<b>It is a part of publication:</b>"
 						+ "<br> Weisong Zhao et al. PANEL: quantitatively mapping reconstruction errors at super-resolution scale by rolling Fourier ring correlation, Nature Methods, X, XXX-XXX (2022)."
 						+ "<br><br><b>Acknowledgements:</b><br>This plugin is for Simplified PANEL (w/o RSM). Please cite PANEL in your publications, if it helps your research."
