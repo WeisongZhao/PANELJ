e15d78fb9ac0b4ebb5a461e1e0eb0d61a969469d
WeisongZhao <zhaoweisong950713@163.com>
2022-11-12 18:32:38 +0800

    nbt

--- src/main/java/com/HIT/weisongzhao/utils/About_.java
+++ src/main/java/com/HIT/weisongzhao/utils/About_.java
@@ -20,10 +20,11 @@ public class About_ extends JDialog implements PlugIn {
 						+"<b>It enables SUPER-RESOLUTION scale:</b>"
 						+"<br>(i) <b>Data uncertainty mapping </b> of image reconstructions <b>without</b> ground-truth (Reconstruction-1 vs Reconstruction-2);"
 						+"<br>(ii) <b>Data uncertainty and leaked model uncertainty mapping</b> of end-to-end deep-learning predictions <b>without</b> ground-truth (Prediction-1 vs Prediction-2);"
-						+"<br>(iii) <b>Full error mapping</b> of reconstructions or predictions <b>with</b> ground-truth (Reconstruction or Prediction vs Ground-truth);"
-						+"<br>(iv) <b> Resolution mapping</b> of raw images (Image-1 vs Image-2)."					
+						+"<br>(iii) <b>Model uncertainty mapping</b> of deep-learning predictions of low-level vision tasks <b>without</b> Ground-Truth (Prediction-1 from model-1 vs Prediction-2 from model-2)"
+						+"<br>(iv) <b>Full error mapping</b> of reconstructions or predictions <b>with</b> ground-truth (Reconstruction or Prediction vs Ground-truth);"
+						+"<br>(v) <b> Resolution mapping</b> of raw images (Image-1 vs Image-2)."					
 						+"<br><br><b>It is a part of publication:</b>"
-						+ "<br> Weisong Zhao et al. Quantitatively mapping local quality at super-resolution scale by rolling Fourier ring correlation, Nature Methods (2022)."
+						+ "<br> Weisong Zhao et al. Quantitatively mapping local quality at super-resolution scale by rolling Fourier ring correlation, Nature Biotechnology (2022)."
 						+ "<br><br><b>Acknowledgements:</b><br>This plugin is for rFRC mapping and simplified PANEL (w/o RSM) pinpointing. Please cite PANEL in your publications, if it helps your research."
 						+ "<br><br><b>Open source:</b><br>https://github.com/WeisongZhao/PANELJ" + "");
 
